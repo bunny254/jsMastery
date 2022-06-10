8f8327fbd7a58a953f135b6f324e7099eda15480
bunny254 <kimaniwachira03@gmail.com>
2022-06-10 16:31:45 +0300

    Juliajames game

---
+++ juliaJames.js
@@ -0,0 +1,15 @@
+let x=1;
+while(x<20){
+    console.log(x);
+    if(x%3===0 && x%5===0){
+        console.log("JuliaJames");
+    }
+    else if(x%3==0){
+        console.log("Julia");
+    }
+    else if(x%5==0){
+        console.log("James");
+    }
+    x=x+1
+    
+}
