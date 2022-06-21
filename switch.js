46dd29d5ad5ca60e215762a411cce04c08051d3a
bunny254 <kimaniwachira03@gmail.com>
2022-06-21 20:02:35 +0300

    switch statement

---
+++ switch.js
@@ -0,0 +1,16 @@
+const a=10
+
+switch(a){
+    case 1:
+        console.log("This is one");
+        break;
+    case 2:
+        console.log("This is two");
+        break;
+    case 3:
+        console.log("This is three");
+        break;
+    case 10:
+        console.log("This is ten");
+        break;
+}
