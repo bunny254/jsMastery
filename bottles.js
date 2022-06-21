754b07452d8288fe113d40b1ce8e3c58c329dc76
bunny254 <kimaniwachira03@gmail.com>
2022-06-21 20:04:45 +0300

    While Loop

---
+++ bottles.js
@@ -0,0 +1,12 @@
+let x=1
+while(x<99){
+    //console.log(x);
+    if(x>1){
+        console.log(`${x}bottles of juice on the wall! ${x} bottles of juice! Take one down, ${x}pass it around...` );
+    }
+    else{
+        console.log(`${x}bottle of juice on the wall! ${x} bottle of juice! Take one down, ${x}pass it around...`)
+
+    }
+    x=x+1
+}
