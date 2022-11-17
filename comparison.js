2d04b0ab32260b85607e2d3e26ff9bbd3df0e3c5
bunny254 <kimaniwachira03@gmail.com>
2022-11-17 05:58:03 +0300

    Comparison

---
+++ comparison.js
@@ -0,0 +1,12 @@
+let a=15;
+let b=10;
+
+const compareValues = ()=>{
+    if (a > b){
+        return "Greater than";
+    }else{
+        return "Less than";
+    }
+}
+
+console.log(compareValues()); 
