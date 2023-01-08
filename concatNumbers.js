d3a3238f1c8dcd16896ad9abc9018e3e99df1a0e
bunny254 <kimaniwachira03@gmail.com>
2023-01-08 17:50:32 +0300

    Concat Numbers and Change String to Number

---
+++ concatNumbers.js
@@ -0,0 +1,6 @@
+let a=3
+let b=3
+
+//Expected outcome =33
+
+console.log(""+a+b);
---
+++ parseInt.js
@@ -0,0 +1,10 @@
+/*
+convert a string value to an integer using the parseInt() function with no second parameter
+*/
+
+let a="10a";
+let b= parseInt(a)
+
+console.log(b);
+let c=3
+console.log(""+2+c);
