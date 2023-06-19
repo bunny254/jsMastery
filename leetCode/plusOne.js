83a6bd479175e131b7ce6a5bde473cc33378800c
Support <lim.supportt@batakenya.batakenya.com>
2023-06-19 14:47:05 +0300

    aws challanges, mergeSortedArray, leet code plus one problem

---
+++ leetCode/plusOne.js
@@ -0,0 +1,24 @@
+let digits = [1]
+
+//test 1
+    //digits =[1,2,3]
+//test 2
+    //digits = [4,3,2,1]
+//test 3
+    //digits = [9]
+
+const plusOne=(digits)=>{
+    for (let i=digits.length-1; i>=0; i-- ){
+        if((digits[i])<9){
+            digits[i] = digits[i]+1;
+            return digits;
+        }else{
+            digits[i]=0;
+        }
+    }
+    digits.unshift(1)
+    return digits;
+
+}
+
+console.log(plusOne(digits));
